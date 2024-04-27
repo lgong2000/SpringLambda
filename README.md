b43f832844b745386f148d6bdf1bd91c8cc75b81
lgong2000 <lgong2000@gmail.com>
2024-04-27 12:24:05 -0500

    Use Lambda DSL in SecurityFilterChain

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ http.authorizeHttpRequests()... works in Spring Boot 3.0.x
     and is deprecated in Spring Boot 3.1 and removed in Spring Boot 3.2
 Version 1
 Old Methods for HttpSecurity
+
+Version 2
+Lambda DSL
+authorizeHttpRequests, csrf, httpBasic all will return HttpSecurity for further use
--- src/main/java/com/example/springlambda/security/SecurityConfig.java
+++ src/main/java/com/example/springlambda/security/SecurityConfig.java
@@ -2,22 +2,38 @@ package com.example.springlambda.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+//    @Bean
+//    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests()
+//                .requestMatchers("/").permitAll()
+//                .requestMatchers("/api/posts/**").hasRole("ADMIN")
+//                .anyRequest().authenticated()
+//                .and()
+//                .csrf().disable()
+//                .httpBasic();
+//        return http.build();
+//    }
+
+    // Lambda DSL
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests()
-                .requestMatchers("/").permitAll()
-                .requestMatchers("/api/posts/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .httpBasic();
-        return http.build();
+    SecurityFilterChain securityFilterChain2(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers("/api/posts/**").hasRole("ADMIN");
+                    auth.anyRequest().authenticated();
+                })
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .build();
     }
 }
